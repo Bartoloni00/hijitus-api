f92b5d8afc211d85f996a196e17b19e16be59d63
Anartz Mugika Ledo <mugan86@gmail.com>
2024-07-27 18:51:51 +0200

    feat: apply pagination with characters endpoint

--- services/ResultsPattern.js
+++ services/ResultsPattern.js
@@ -1,15 +1,16 @@
 export default class Result {
-    constructor(success, data = undefined, error = undefined) {
+    constructor(success, data = undefined, pagination = undefined, error = undefined) {
         this.success = success;
+        this.pagination = pagination;
         this.data = data;
         this.error = error;
     }
 
-    static success(data) {
-        return new Result(true, data);
+    static success(data, pagination) {
+        return new Result(true, data, pagination);
     }
 
     static failure(error) {
-        return new Result(false, undefined, error);
+        return new Result(false, undefined, undefined, error);
     }
 }
